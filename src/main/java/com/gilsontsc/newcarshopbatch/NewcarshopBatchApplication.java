3bc8d956fcb38934db897d25428a85b6afd8e4a2
Gilson <gilson.costa@dce.ufpb.br>
2020-08-21 19:37:07 -0300

    Criação do projeto

---
+++ src/main/java/com/gilsontsc/newcarshopbatch/NewcarshopBatchApplication.java
@@ -0,0 +1,13 @@
+package com.gilsontsc.newcarshopbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcarshopBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcarshopBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilsontsc/newcarshopbatch/NewcarshopBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilsontsc.newcarshopbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcarshopBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
