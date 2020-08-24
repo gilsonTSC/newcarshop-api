d9a0ccc83590444e6babdc712e7676af721a9d95
Gilson <gilson.costa@dce.ufpb.br>
2020-08-24 20:48:02 -0300

    Implementando o CarroItemReader

---
+++ src/main/java/com/gilsontsc/newcarshopbatch/configuration/chunklet/CarroItemReader.java
@@ -0,0 +1,44 @@
+package com.gilsontsc.newcarshopbatch.configuration.chunklet;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+
+import com.gilsontsc.newcarshopbatch.dto.CarroDto;
+
+public class CarroItemReader implements ItemReader<CarroDto>, StepExecutionListener{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CarroItemReader.class);
+	private Iterator<CarroDto> carroInterator;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		ExecutionContext ec = stepExecution.getJobExecution().getExecutionContext();
+		List<CarroDto> carroDtoList = (List<CarroDto>) ec.get("carroInList");
+		this.carroInterator = carroDtoList.iterator();
+		LOGGER.info("Iniciando o READER...");
+	}
+	
+	@Override
+	public CarroDto read() {
+		if(this.carroInterator != null && this.carroInterator.hasNext()) {
+			return this.carroInterator.next();
+		}
+		return null;
+	}
+
+	@Override
+	public ExitStatus afterStep(StepExecution stepExecution) {
+		LOGGER.info("Finalizando o READER...");
+		return ExitStatus.COMPLETED;
+	}
+
+}
